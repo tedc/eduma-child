ec6a9d0582f40a01e242c44dd39c2de6e8dec9ce
tedc <egrandinetti@gmail.com>
2017-06-28 13:19:59 +0200

    send

--- widgets/learn-press-categories/tpl/simple.php
+++ widgets/learn-press-categories/tpl/simple.php
@@ -7,7 +7,7 @@
 		$terms = get_terms(
 			array(
 				'taxonomy' => 'course_category',
-				'number' => $instance['number'] || 0,
+				'number' => $instance['number'] || 7,
 				'orderby' => 'name'
 			)
 		); 
@@ -16,13 +16,14 @@
 	<div class="learn-press-categories__cell">
 		<?php
 			$img_id = get_term_meta( $term->term_id, 'thim_learnpress_top_image', true )['id'];
-			var_dump(get_term_meta( $term->term_id, 'thim_learnpress_top_image', true ));
 			$image = wp_get_attachment_image_src( $img_id, 'large', false ); ?>
 		<figure class="learn-press-categories__figure" style="background-image: url(<?php echo $image[0]; ?>)">
 			<img src="<?php echo $image[0]; ?>"  class="learn-press-categories__image" />
 		</figure>
+		<div class="learn-press-categories__content">
 		<h3 class="learn-press-categories__title"><?php echo $term->name; ?></h3>
-		
+		<a href="<?php echo get_term_link( $term->term_id); ?>" class="learn-press-categories__button"><?php _e('Scopri', 'uba'); ?></a>
+		</div>
 	</div>
 	<?php endforeach; ?>
 </div>
