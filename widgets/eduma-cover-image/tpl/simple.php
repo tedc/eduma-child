bce03daeafb222e89f8736e76833bd2e8be45e5e
tedc <egrandinetti@gmail.com>
2017-06-29 19:58:43 +0200

    image

--- widgets/eduma-cover-image/tpl/simple.php
+++ widgets/eduma-cover-image/tpl/simple.php
@@ -3,6 +3,6 @@
 	$position = $instance['position'];
 	$type = $instance['type'];
 ?>
-<figure class="eduma-child__figure eduma-child__figure--<?php echo $type; ?>-<?php echo $position; ?>" style="background-image: url(<?php echo $image[0]; ?>)">
+<figure class="eduma-child__figure eduma-child__figure--<?php echo $position; ?> eduma-child__figure--<?php echo $type; ?>" style="background-image: url(<?php echo $image[0]; ?>)">
 	<?php echo wp_get_attachment_image( $instance['image'], 'large', false ); ?>
 </figure>
