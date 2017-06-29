0e67b58bd83e8236dab87313193bf126ff129a2d
tedc <egrandinetti@gmail.com>
2017-06-29 19:09:42 +0200

    type fix

--- widgets/eduma-cover-image/tpl/simple.php
+++ widgets/eduma-cover-image/tpl/simple.php
@@ -1,8 +1,8 @@
 <?php 
 	$image = wp_get_attachment_image_src( $instance['image'];, 'full', false );
 	$position = $instance['position'];
-	$position = $instance['type'];
+	$type = $instance['type'];
 ?>
 <figure class="eduma-child__figure eduma-child__figure--<?php echo $type; ?>-<?php echo $position; ?>" style="background-image: url(<?php echo $image[0]; ?>)">
-	<?php echo wp_get_attachment_image( $instance['image'], 'large' ); ?>
+	<?php echo wp_get_attachment_image( $instance['image'], 'large', false ); ?>
 </figure>
