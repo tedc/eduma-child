b624c1652fdcf5ce4ff62ef018b09270d24d7e36
tedc <egrandinetti@gmail.com>
2017-06-30 10:49:30 +0200

    latest courses

---
+++ widgets/learn-press-courses/learn-press-courses.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+Widget Name: Learn Press Courses
+Description: Widget per inserire la lista degli ultimi corsi.
+Author: Besposken Studio
+Author URI: http://www.bspkn.it
+*/
+
+class Learn_Press_Courses extends SiteOrigin_Widget  {
+	function get_template_name($instance) {
+        return 'simple';
+    }
+
+    function get_style_name($instance) {
+        return 'tpl';
+    }
+    function __construct() {
+		parent::__construct(
+			'learn-press-courses',
+			__('Corsi: le categorie', 'siteorigin-panels'),
+			array(
+				'description' => __('Mostra la lista degli ultimi corsi', 'siteorigin-panels'),
+				'default_style' => 'simple',
+				'panels_groups' => array('mytheme')
+			),
+			array(),
+			array(
+				'title' => array(
+					'type' => 'text',
+					'label' => __('Titolo', 'siteorigin-panels'),
+				),
+				'number' => array(
+					'type' => 'number',
+					// TRANSLATORS: Uniform Resource Locator
+					'label' => __('Quantità', 'siteorigin-panels'),
+				),
+			),
+			get_template_directory()
+		);		
+		//$this->add_sub_widget('button', __('Button', 'siteorigin-panels'), 'SiteOrigin_Panels_Widget_Button');
+	}
+}
+
+siteorigin_widget_register('learn-press-courses', __FILE__, 'Learn_Press_Courses');
+
---
+++ widgets/learn-press-courses/tpl/simple.php
@@ -0,0 +1,32 @@
+<div class="latest-courses">
+<?php if($instance['title']) { ?>
+
+<?php } 
+	$per_page = ($instance['number']) ? $instance['number'] : 3;
+	$latest = new WP_Query(
+		array(
+			'post_type' => 'lp_course',
+			'posts_per_page' => $per_page
+		)
+	);
+	if($latest->have_posts()) :
+?> 
+<ul class="latest-courses__list">
+	<?php while($latest->have_posts()) : $latest->the_post(); ?>
+	<li class="latest-courses__item">
+	<header class="latest-courses__header">
+	<?php
+		$cats = wp_get_post_terms(get_the_ID(), 'course_category');
+		$c = 0;
+		foreach ($cats as $cat) {
+			$comma = ($c < count($cats) - 1) ? ', ' : '';
+			echo '<a href="'.get_term_link( $cat->term_id, 'course_category' ).'" class="latest-courses__cat">'.$cat->name.'</a>'.$comma;
+		}
+	?>
+	<h3 class="latest-courses__title"><a href="<?php the_permalink(  ); ?>"><?php the_title(); ?><</a></h3>
+	</header>
+	<footer class="latest-courses__button button-inverted"><div class="thim-widget-button thim-widget-button-base"><a class="widget-button tiny-rounded normal" href="<?php the_permalink(  ); ?>"><?php _e('Scopri', 'uba'); ?></a></div></footer>
+	</li>
+	<?php endwhile; wp_reset_query(); ?>
+</ul>
+<?php endif; ?>
