a16344ac74ae9a2975ff7407d7cf02930daa2e64
tedc <egrandinetti@gmail.com>
2017-06-30 16:42:20 +0200

    color

--- widgets/icon-link/tpl/simple.php
+++ widgets/icon-link/tpl/simple.php
@@ -1,9 +1,9 @@
 <a href="<?php echo $instance['link']; ?>" class="<?php echo $instance['icon']; ?> icon-<?php echo sanitize_title( $instance['link']); ?>"<?php echo ($instance['blank']) ? ' target="_blank"' : ''; ?>></a>
 <style>
 .<?php echo $instance['icon']; ?>.icon-<?php echo sanitize_title( $instance['link']); ?> {
-	color: <?php echo $instance['color']; ?>
+	color: <?php echo $instance['color']; ?> !important;
 }
 .<?php echo $instance['icon']; ?>.icon-<?php echo sanitize_title( $instance['link']); ?>:hover {
-	color: <?php echo $instance['color_hover']; ?>
+	color: <?php echo $instance['color_hover']; ?> !important;
 }
 </style>
