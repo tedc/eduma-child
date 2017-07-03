f900bfce810373309bf2d080c5c1a1c75dbc1ba9
tedc <egrandinetti@gmail.com>
2017-07-03 15:45:22 +0200

    id="icon-thumbnail-src"

--- inc/admin/views/admin.php
+++ inc/admin/views/admin.php
@@ -3,7 +3,7 @@
 </p>
 
 <div id="featured-icon-image-container" class="hidden">
-	<img src="<?php if(isset($term->term_id)) { echo get_term_meta( $term->term_id, 'icon-thumbnail-src', true ); } ?>" />
+	<img src="<?php if(isset($term->term_id)) { echo get_term_meta( $term->term_id, 'icon-thumbnail-src', true ); } ?>" id="icon-thumbnail-src" />
 </div><!-- #featured-icon-image-container -->
 
 <p class="hide-if-no-js hidden">
