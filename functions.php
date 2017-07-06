aff894cd60a8a15945a092a8e4c0ce216f41b435
tedc <egrandinetti@gmail.com>
2017-07-06 18:15:28 +0200

    search by cat

--- functions.php
+++ functions.php
@@ -51,18 +51,16 @@ add_filter('siteorigin_panels_widget_dialog_tabs', 'mytheme_add_widget_tabs', 20
 add_action('pre_get_posts', 'search_by_cat');
 function search_by_cat($query)
 {
-    if (is_page(get_option('learn_press_courses_page_id'))) {
-        var_dump($query);
-        $cat = intval($_GET['course_category']);
-        $arr = ($cat > 0) ? array(
-        	array(
-        		'taxonomy' => 'course_category',
-        		'field' => 'term_id',
-        		'term' => $cat
-        	)
-        ) : false;
-        $query->set('tax_query', $arr);
-    }
+    var_dump($query);
+    $cat = intval($_GET['course_category']);
+    $arr = ($cat > 0) ? array(
+        array(
+            'taxonomy' => 'course_category',
+            'field' => 'term_id',
+            'term' => $cat
+        )
+    ) : false;
+    $query->set('tax_query', $arr);
 }
 
 include( locate_template( 'inc/add-icon-image.php', false, true ));
