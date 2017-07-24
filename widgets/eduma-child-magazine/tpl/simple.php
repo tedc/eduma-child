71926102a3fd56def69d3983e3b4495ee9963ebe
tedc <egrandinetti@gmail.com>
2017-07-24 17:23:43 +0200

    var_dump($first);

--- widgets/eduma-child-magazine/tpl/simple.php
+++ widgets/eduma-child-magazine/tpl/simple.php
@@ -31,11 +31,12 @@
 		</div>
 	</div>
 	<?php endwhile; wp_reset_query();
-	var_dump($first);
 	 ?>
 	<div class="eduma-magazine__grid">
 	<?php
 		$num = ($instance['number']) ? $instance['number'] : 2;
+		var_dump($first, $num);
+	
 		$new_q = new WP_Query(
 			array(	
 				'post__not_in' => $first,
